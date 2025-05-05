d6eed6048637495bd1d52b232db3af9442321854
Mohammed-Imthihyaz <141853784+Mohammed-Imthihyaz@users.noreply.github.com>
2025-05-05 17:23:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,57 +1,32 @@
 import cors from "cors";
 import express from "express";
 import { connectDB } from "./db/connectDB.js";
+
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import codeRoutes from "./routes/codeExecution.route.js";
-import dotenv from "dotenv";
-
-dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT || 5001;
-
-const allowedOrigins = [
-  "https://byte-code-yt22-ajvyfelw4-mohammed-imthihyazs-projects.vercel.app",
-  "http://localhost:5173"
-];
+const PORT = process.env.PORT|| 5001;
 
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like mobile apps or curl)
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true
-  })
-);
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 
-app.use((req, res, next) => {
-  res.setHeader("Cache-Control", "no-store");
-  next();
-});
-
-
-app.use(express.json());
+// Use middleware to parse JSON
+app.use(express.json()); // Corrected to use parentheses
 app.use(cookieParser());
+app.use("/api/auth", authRoutes); // Un-comment this if you want to use auth routes
+app.use("/api/codes",codeRoutes);
 
 
-app.use("/api/auth", authRoutes);
-app.use("/api/codes", codeRoutes);
-
-
-app.get("/", (req, res) => {
-  res.status(200).send("Welcome to root URL of Server");
+app.get('/', (req, res) => {
+    res.status(200).send("Welcome to root URL of Server");
 });
 
-
-app.listen(PORT, async () => {
-  await connectDB();
-  console.log("✅ Server running on port", PORT);
+app.listen(PORT, (error) => {
+    connectDB();
+    if (!error)
+        console.log("Server is Successfully Running, and App is listening on port " + PORT);
+    else
+        console.log("Error occurred, server can't start", error);
 });
